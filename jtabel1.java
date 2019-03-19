e4121dd38102237fae9e4ca53dc5cab1d74ec1b5
nadhifatulmuna <48705313+nadhifatulmuna@users.noreply.github.com>
2019-03-19 00:40:59 -0700

    Add files via upload

---
+++ jtabel1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyek;
+
+/**
+ *
+ * @author nadhif
+ */
+class jtabel1 {
+
+    static int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ tabel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyek;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author nadhif
+ */
+    
+
